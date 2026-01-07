f9fe2d255ba2d6b3916b6f3c322e7bb93acd1916
hwarang song <shr8596@gmail.com>
2026-01-07 15:46:43 +0900

    main.py에 post 함수 추가(테스트용이라 삭제할 예정)

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 def read_root():
 	return {"message": "Hello, adepterz!"}
+
+@app.post("/user")
+def read_root(user_name: str):
+	return {
+		"message": "Hello, adepterz!",
+		"name": f"{user_name}",
+	}
