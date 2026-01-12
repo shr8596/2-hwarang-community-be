1a2940eddf2fd749766a589a2f968335e4c71bd9
hwarang song <shr8596@gmail.com>
2026-01-12 16:35:05 +0900

    prefix 삭제(__init__.py에 있음)

--- routes/users/user_router.py
+++ routes/users/user_router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from controllers.users import user_controller
 
-router = APIRouter(prefix="/users")
+router = APIRouter()
 
 
 # 특정 사용자 상세 조회
-@router.get("/me/{user_id}")
+@router.get("/me{user_id}")
 def get_user_info(user_id: str):
     return user_controller.get_user_info(user_id)
