b7d1a47bc038fc752ed6dfb09703a0799287b438
hwarang song <shr8596@gmail.com>
2026-01-13 19:06:32 +0900

    로그인, 로그아웃 라우팅

--- routes/users/user_router.py
+++ routes/users/user_router.py
@@ -1,8 +1,17 @@
-from fastapi import APIRouter
 from controllers.users import user_controller
+from fastapi import APIRouter, Request
 
 router = APIRouter()
 
+# 회원 로그인
+@router.post("/login")
+async def login(request: Request):
+    return await user_controller.login_user(request)
+
+# 회원 로그아웃
+@router.delete("/logout")
+async def logout(request: Request):
+    return await user_controller.logout_user(request)
 
 # 회원 정보 조회
 @router.get("/me/{user_id}")
