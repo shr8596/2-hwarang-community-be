493d43d80ea5333868a4566564b3c3719a893545
hwarang song <shr8596@gmail.com>
2026-01-14 15:30:15 +0900

    메시지를 모아둔 파일을 성공/실패 구분

--- constants/error_message.py
+++ constants/error_message.py
@@ -1,7 +1,3 @@
-# 성공 메시지
-def successfully(field_name: str) -> str:
-    return f"{field_name}_successfully"
-
 # 실패 메시지
 # 400
 missing_required_field = "missing_required_field"
---
+++ constants/success_message.py
@@ -0,0 +1,3 @@
+# 성공 메시지
+def successfully(field_name: str) -> str:
+    return f"{field_name}_successfully"
