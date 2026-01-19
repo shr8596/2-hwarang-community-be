3ef57a7c7cc6ab5aafb3e4b37be925b99122bf1a
hwarang song <shr8596@gmail.com>
2026-01-19 09:44:56 +0900

    공통 스키마에 설명 주석 추가

--- utils/response_schema.py
+++ utils/response_schema.py
@@ -1,7 +1,8 @@
 from typing import Any
 
 
-def response_dict(message: str, data: Any = None) -> dict:
+# API 응답 시 JSONResponse(성공), HTTPException(실패)에서 공통된 응답 스키마를 정의
+def response_schema(message: str, data: Any = None) -> dict:
     return {
         "message"   : message,
         "data"      : data,
